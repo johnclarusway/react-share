617a14ee8547480bb6b3e19a5a7977ecd3e334ab
ms <ms@gmail.com>
2020-11-26 21:22:22 +0200

    Add formik to sign in page

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, TextField, Grid, Container } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import firebase from "../firebase/firebase.utils";
+import { Formik } from "formik";
 
 const stylesFunc = makeStyles({
   wrapper: {
@@ -9,51 +10,73 @@ const stylesFunc = makeStyles({
   },
 });
 
+const initialValues = {
+  email: "",
+  password: "",
+};
+
 function Signin() {
-  const signupStyles = stylesFunc();
+  const signinStyles = stylesFunc();
 
   const handleGoogleButtonClick = () => {
     firebase.useGoogleProvider();
   };
 
+  const handleFormSubmit = (values) => {
+    alert(JSON.stringify(values, null, 2));
+  };
+
   return (
-    <Container className={signupStyles.wrapper} maxWidth="sm">
-      <form>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <TextField
-              name="email"
-              label="Email"
-              variant="outlined"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              name="password"
-              label="Password"
-              variant="outlined"
-              type="password"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button type="submit" variant="contained" color="primary" fullWidth>
-              Login
-            </Button>
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              variant="contained"
-              color="primary"
-              fullWidth
-              onClick={handleGoogleButtonClick}
-            >
-              Sign In with Google
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
+    <Container className={signinStyles.wrapper} maxWidth="sm">
+      <Formik initialValues={initialValues} onSubmit={handleFormSubmit}>
+        {({ handleSubmit, handleChange, values }) => (
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <TextField
+                  name="email"
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  value={values.email}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="password"
+                  label="Password"
+                  variant="outlined"
+                  type="password"
+                  fullWidth
+                  value={values.password}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                >
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  onClick={handleGoogleButtonClick}
+                >
+                  Sign In with Google
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        )}
+      </Formik>
     </Container>
   );
 }
