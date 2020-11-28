4a15572dfb9480fe1bdb385f24ff76cca549e132
ms <ms@gmail.com>
2020-11-28 20:48:51 +0200

    Add date-fns

--- src/pages/UserDetail.js
+++ src/pages/UserDetail.js
@@ -4,6 +4,7 @@ import { fetchData } from "../helper/FetchData";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container, Grid, capitalize } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
+import { format as formatDate, parseISO } from "date-fns";
 
 const stylesFunc = makeStyles((theme) => ({
   wrapper: {
@@ -30,11 +31,18 @@ function UserDetail() {
 
   return (
     <Container className={mainStyles.wrapper}>
-      <img src={userDetail.picture} alt="user" />
-      <Typography variant="h4">{userDetail.firstName}</Typography>
-      <Typography variant="h4">{userDetail.lastName}</Typography>
-      <Typography variant="h4">{userDetail.registerDate}</Typography>
-      <Typography variant="h4">{userDetail.phone}</Typography>
+      <img src={userDetail?.picture} alt="user" />
+      <Typography variant="h4">{userDetail?.firstName}</Typography>
+      <Typography variant="h4">{userDetail?.lastName}</Typography>
+      {userDetail?.registerDate && (
+        <Typography variant="h4">
+          {
+            //TODO: move to helper
+          }
+          {formatDate(parseISO(userDetail.registerDate), "MMM/dd/yy")}
+        </Typography>
+      )}
+      <Typography variant="h4">{userDetail?.phone}</Typography>
     </Container>
   );
 }
