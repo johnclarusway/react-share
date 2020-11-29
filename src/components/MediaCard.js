7efbef5d4a2a9a0db09beaca77006023252be221
Hakan <67585490+hakansaglam29@users.noreply.github.com>
2020-11-29 22:42:44 +0100

    added style to main page

--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -29,6 +29,11 @@ const useStyles = makeStyles({
     width: "auto",
     resizeMode: "contain",
   },
+  ellipsis: {
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+  },
 });
 
 export default function MediaCard({ id, userImage, userName, userEmail }) {
@@ -40,10 +45,10 @@ export default function MediaCard({ id, userImage, userName, userEmail }) {
       <CardActionArea onClick={() => history.push(`/user/${id}`)}>
         <CardMedia className={classes.media} image={userImage} />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
+          <Typography className={classes.ellipsis} gutterBottom variant="h6" component="h2">
             {userName}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <Typography className={classes.ellipsis} variant="body2" color="textSecondary" component="p">
             {userEmail}
           </Typography>
         </CardContent>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -14,7 +14,9 @@ const stylesFunc = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     minHeight: "100vh",
+    maxWidth:"85%",
     marginTop: "5rem",
+    marginBottom:"2rem",
     textAlign: "center",
   },
   avatar: {
@@ -50,7 +52,7 @@ function Main() {
         <Grid container spacing={1}>
           {userList?.map((user) => {
             return (
-              <Grid item sm={4} xs={6} key={user?.id}>
+              <Grid item lg={3} md={4} sm={6} xs={12} key={user?.id}>
                 <MediaCard
                   id={user.id}
                   userImage={user?.picture}
