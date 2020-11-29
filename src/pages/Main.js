06ab0df9134512a8fa95848804d9f7345630a854
E2029-Aydin <aydinsanoz2@gmail.com>
2020-11-29 10:52:23 +0300

    axios func moved  to helper

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,59 +1,52 @@
-import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Container, Grid, capitalize } from "@material-ui/core";
-import axios from "axios";
-import MediaCard from "../components/MediaCard";
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Grid, capitalize } from '@material-ui/core';
+import axios from 'axios';
+import MediaCard from '../components/MediaCard';
+import { fetchData } from '../helper/FetchData';
 
 const stylesFunc = makeStyles((theme) => ({
-  wrapper: {
-    marginTop: "10rem",
-    height: "calc(100vh - 19.0625rem)",
-    textAlign: "center",
-  },
-  avatar: {
-    margin: "1rem auto",
-    backgroundColor: theme.palette.secondary.main,
-  },
+	wrapper: {
+		marginTop: '10rem',
+		height: 'calc(100vh - 19.0625rem)',
+		textAlign: 'center',
+	},
+	avatar: {
+		margin: '1rem auto',
+		backgroundColor: theme.palette.secondary.main,
+	},
 }));
 
 function Main() {
-  const [userList, setUserList] = useState();
-  const mainStyles = stylesFunc();
-  const { REACT_APP_API_BASE_URL, REACT_APP_API_TOKEN } = process.env;
+	const [userList, setUserList] = useState();
+	const mainStyles = stylesFunc();
+	const { REACT_APP_API_BASE_URL, REACT_APP_API_TOKEN } = process.env;
 
-  const fetchData = async () => {
-    const response = await axios.get(`${REACT_APP_API_BASE_URL}/user`, {
-      headers: {
-        "app-id": REACT_APP_API_TOKEN,
-      },
-    });
-    setUserList(response?.data?.data);
-  };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+	useEffect(() => {
+		fetchData('/user').then((response) => setUserList(response.data));
+	}, []);
 
-  return (
-    <Container className={mainStyles.wrapper}>
-      <Grid container spacing={1}>
-        {userList?.map((user) => {
-          return (
-            <Grid item sm={4} xs={6} key={user?.id}>
-              <MediaCard
-                id={user.id}
-                userImage={user?.picture}
-                userName={`${capitalize(user?.title)} ${user?.firstName} ${
-                  user?.lastName
-                }`}
-                userEmail={user?.email}
-              />
-            </Grid>
-          );
-        })}
-      </Grid>
-    </Container>
-  );
+	return (
+		<Container className={mainStyles.wrapper}>
+			<Grid container spacing={1}>
+				{userList?.map((user) => {
+					return (
+						<Grid item sm={4} xs={6} key={user?.id}>
+							<MediaCard
+								id={user.id}
+								userImage={user?.picture}
+								userName={`${capitalize(user?.title)} ${
+									user?.firstName
+								} ${user?.lastName}`}
+								userEmail={user?.email}
+							/>
+						</Grid>
+					);
+				})}
+			</Grid>
+		</Container>
+	);
 }
 
 export default Main;
