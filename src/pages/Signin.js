1da90a16339b56a931a152e5c8e69be996b95cfb
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-12-07 00:59:16 +0000

    Added 'Don't have an account? Register', some styles, redirect home page after signing in with Google

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -22,9 +22,10 @@ const signInValidationSchema = Yup.object().shape({
 
 const stylesFunc = makeStyles((theme) => ({
   wrapper: {
-    marginTop: "10rem",
+    marginTop: "3rem",
     height: "calc(100vh - 19.0625rem)",
     textAlign: "center",
+    marginBottom:"9rem",
   },
   avatar: {
     margin: "1rem auto",
@@ -32,7 +33,13 @@ const stylesFunc = makeStyles((theme) => ({
   },
   signIn: {
     margin: "1rem",
-  },
+  }, 
+  register: {
+    textDecoration: 'none',
+    fontWeight: '600',
+    paddingLeft : '0.5rem'
+  }
+  
 }));
 
 const initialValues = {
@@ -47,6 +54,8 @@ function Signin() {
 
   const handleGoogleButtonClick = () => {
     firebase.useGoogleProvider();
+    alert('You are succesfully logged in!');
+    history.push('/');
   };
 
   const handleFormSubmit = (values) => {
@@ -129,7 +138,12 @@ function Signin() {
             //TODO: Add register & forgot password text & links
             */}
             <p>
-              <a href="/forgot-password">Forgot Password?</a>
+              Don't have an account?      
+              <a className = {signinStyles.register} href="/register">Register</a>
+            </p>
+            
+            <p>
+              <a className = {signinStyles.register} href="/forgot-password">Forgot Password?</a>
             </p>
           </form>
         )}
